cec98f0bc8fc1ab2278ec4f36ba3e26d96b37a68
Brendon Bitencourt Braga <brendon.braga@gmail.com>
2019-11-30 22:50:21 -0300

    Remove Extras Comments

--- Artable/XIBs/ForgotPasswordViewController.swift
+++ Artable/XIBs/ForgotPasswordViewController.swift
@@ -15,7 +15,6 @@ class ForgotPasswordViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -36,15 +35,5 @@ class ForgotPasswordViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
